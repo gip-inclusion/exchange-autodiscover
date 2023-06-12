1b6f39d2466b7a04f5944ae1e90e66139f3d1a43
Jonathan Perret <j-github@jonathanperret.net>
2023-06-12 12:49:32 +0200

    basic autodiscover XML

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+require('express-async-errors');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
+const app = express();
+const { engine } = require('express-handlebars');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.raw({ type: '*/*' }));
 
-app.use('/', indexRouter);
 app.use('/autodiscover', require('./routes/autodiscover'));
+
+app.locals.layout = null;
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/autodiscover.js
+++ routes/autodiscover.js
@@ -1,10 +1,45 @@
-const express = require('express');
+const express = require("express");
+const xml2js = require("xml2js");
 const router = express.Router();
 
 /* GET home page. */
-router.post('/autodiscover.xml', function(req, res, next) {
-  res.end("coucou");
+router.post("/autodiscover.xml", async function (req, res, next) {
+  const isHTTPS = req.headers["x-forwarded-proto"] === "https";
+
+  if (!isHTTPS) {
+    return res.redirect(
+      "https://inclusion-autodiscover-test.osc-fr1.scalingo.io/Autodiscover/Autodiscover.xml"
+    );
+  }
+
+  const {
+    Autodiscover: {
+      Request: [
+        {
+          AcceptableResponseSchema: [schema],
+          EMailAddress: [email],
+        },
+      ],
+    },
+  } = await xml2js.parseStringPromise(req.body.toString("utf-8"));
+
+  const [username, domain] = email.split("@");
+
+  res.set("content-type", "application/xml");
+  res.render("autodiscover", {
+    schema,
+    email,
+    username,
+    domain,
+    smtp: {
+      host: "ex5.mail.ovh.net",
+      port: 587,
+    },
+    imap: {
+      host: "ex5.mail.ovh.net",
+      port: 993,
+    },
+  });
 });
 
 module.exports = router;
-
